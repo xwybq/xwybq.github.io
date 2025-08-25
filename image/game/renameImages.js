4efd3692e9ead47a34ebcd160e25946699ba85e3
xwybq <3290819205@qq.com>
2025-08-25 13:48:15 +0800

    Site updated: 2025-08-25 13:48:14

--- image/game/renameImages.js
+++ image/game/renameImages.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // 配置：指定图片文件夹路径和需要处理的图片扩展名
 const config = {
-  folderPath: './战神', // 图片文件夹路径，可修改为实际路径
+  folderPath: './饿殍：明末千里行', // 图片文件夹路径，可修改为实际路径
   imageExtensions: ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'] // 支持的图片格式
 };
 
